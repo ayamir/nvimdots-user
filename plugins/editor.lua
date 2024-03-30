e1a0959781c08a9ac688545a27a91a9faff2d24b
ayamir <lgt986452565@gmail.com>
2024-03-30 23:46:14 +0800

    feat: enable persisted.nvim, add nvim-ufo.

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -1,8 +1,8 @@
 local editor = {}
 
--- editor["olimorris/persisted.nvim"] = {
--- 	lazy = false,
--- }
+editor["olimorris/persisted.nvim"] = {
+	lazy = false,
+}
 editor["max397574/better-escape.nvim"] = {
 	lazy = true,
 	event = { "CursorHold", "CursorHoldI" },
@@ -11,6 +11,22 @@ editor["max397574/better-escape.nvim"] = {
 editor["chrisgrieser/nvim-spider"] = {
 	lazy = true,
 	event = { "CursorHold", "CursorHoldI" },
+}
+editor["kevinhwang91/nvim-ufo"] = {
+	lazy = true,
+	event = { "BufReadPost" },
+	dependencies = {
+		{ "luukvbaal/statuscol.nvim", branch = "0.10", config = require("configs.editor.statuscol") },
+		{ "kevinhwang91/promise-async" },
+	},
+	config = require("configs.editor.ufo"),
+	init = function()
+		vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+		vim.o.foldcolumn = "1" -- '0' is not bad
+		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+	end,
 }
 
 return editor
