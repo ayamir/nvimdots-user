44a393b521be391f066067b713da9f8ff45b6aa8
ayamir <lgt986452565@gmail.com>
2024-06-30 11:02:57 +0800

    chore: remove dependency for nvim_rocks.

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -11,13 +11,6 @@ editor["max397574/better-escape.nvim"] = {
 editor["chrisgrieser/nvim-spider"] = {
 	lazy = true,
 	event = { "CursorHold", "CursorHoldI" },
-	dependencies = {
-		"theHamsta/nvim_rocks",
-		build = "pip3 install --user hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
-		config = function()
-			require("nvim_rocks").ensure_installed("luautf8")
-		end,
-	},
 }
 
 return editor
