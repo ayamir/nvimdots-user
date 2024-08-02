58200cf4f491978896dae0f0657642f9cf2c4045
ayamir <lgt986452565@gmail.com>
2024-08-02 11:47:02 +0800

    feat: add keymap.

--- keymap/ui.lua
+++ keymap/ui.lua
@@ -3,4 +3,5 @@ local map_callback = require("keymap.bind").map_callback
 
 return {
 	["n|<C-n>"] = map_cr("Neotree toggle"):with_noremap():with_silent():with_desc("filetree: Toggle"),
+	["n|<A-S-q>"] = map_cr("BufferLineCloseOthers"):with_noremap():with_silent():with_desc("buffer: close others"),
 }
