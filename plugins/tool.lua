07a5903d0bb9d8bd5fe400f2d5fedbfc91fc76cb
ayamir <lgt986452565@gmail.com>
2024-08-02 16:29:03 +0800

    feat: update leecode config.

--- plugins/tool.lua
+++ plugins/tool.lua
@@ -65,6 +65,17 @@ tool["kawre/leetcode.nvim"] = {
 			translator = false, ---@type boolean
 			translate_problems = true, ---@type boolean
 		}, -- configuration goes here
+		description = {
+			position = "left",
+			width = "20%",
+			show_stats = true,
+		},
+		injector = {
+			["cpp"] = {
+				before = { "#include <vector>", "#include <iostream>", "using namespace std;" },
+				after = "int main() { Solution s; return 0; }",
+			},
+		},
 	},
 }
 
