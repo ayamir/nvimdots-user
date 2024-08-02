d50a4d3918f604a349b46cda489a7dd2436c982b
ayamir <lgt986452565@gmail.com>
2024-08-02 11:41:41 +0800

    fix: remove nvim-tree relevant config.

---
+++ configs/toggleterm.lua
@@ -0,0 +1,8 @@
+return {
+	on_open = function()
+		-- Prevent infinite calls from freezing neovim.
+		-- Only set these options specific to this terminal buffer.
+		vim.api.nvim_set_option_value("foldmethod", "manual", { scope = "local" })
+		vim.api.nvim_set_option_value("foldexpr", "0", { scope = "local" })
+	end,
+}
