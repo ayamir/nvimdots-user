1a10e0dde95549544fe3bfcbdc0885d4d88d9be6
ayamir <lgt986452565@gmail.com>
2024-08-12 17:48:14 +0800

    feat: add g++-14 to clangd find list.

---
+++ configs/lsp-servers/clangd.lua
@@ -0,0 +1,34 @@
+local function get_binary_path_list(binaries)
+	local path_list = {}
+	for _, binary in ipairs(binaries) do
+		local path = vim.fn.exepath(binary)
+		if path ~= "" then
+			table.insert(path_list, path)
+		end
+	end
+	return table.concat(path_list, ",")
+end
+
+return function(options)
+	require("lspconfig").clangd.setup({
+		on_attach = options.on_attach,
+		capabilities = vim.tbl_deep_extend("keep", { offsetEncoding = { "utf-16", "utf-8" } }, options.capabilities),
+		single_file_support = true,
+		cmd = {
+			"clangd",
+			"-j=12",
+			"--enable-config",
+			"--background-index",
+			"--pch-storage=memory",
+			-- You MUST set this arg ↓ to your c/cpp compiler location (if not included)!
+			"--query-driver=" .. get_binary_path_list({ "clang++", "clang", "gcc", "g++", "g++-14" }),
+			"--clang-tidy",
+			"--all-scopes-completion",
+			"--completion-style=detailed",
+			"--header-insertion-decorators",
+			"--header-insertion=iwyu",
+			"--limit-references=3000",
+			"--limit-results=350",
+		},
+	})
+end
