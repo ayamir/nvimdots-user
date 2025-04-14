ab71027b186a582da270ccdf1ef6ec070a07ca26
ayamir <lgt986452565@gmail.com>
2025-04-14 23:43:17 +0800

    tidy config.

--- plugins/completion.lua
+++ plugins/completion.lua
@@ -11,6 +11,16 @@ completion["rachartier/tiny-inline-diagnostic.nvim"] = {
 	config = function()
 		require("tiny-inline-diagnostic").setup({
 			preset = "powerline",
+			options = {
+				show_source = {
+					enabled = true,
+					if_many = true,
+				},
+				use_icons_from_diagnostic = true,
+				break_line = {
+					enabled = true,
+				},
+			},
 		})
 	end,
 }
