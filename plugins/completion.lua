7628b5db88e48077c8920d988438b6f25c1cbaf5
ayamir <lgt986452565@gmail.com>
2025-04-18 10:38:22 +0800

    update.

--- plugins/completion.lua
+++ plugins/completion.lua
@@ -5,6 +5,16 @@ completion["git@code.byted.org:chenjiaqi.cposture/codeverse.vim.git"] = {
 		require("marscode").setup()
 	end,
 }
+completion["ayamir/garbage-day.nvim"] = {
+	lazy = true,
+	event = "LspAttach",
+	config = function()
+		require("modules.utils").load_plugin("garbage-day", {
+			excluded_lsp_clients = { "null-ls" },
+			notifications = true,
+		})
+	end,
+}
 completion["rachartier/tiny-inline-diagnostic.nvim"] = {
 	event = "VeryLazy",
 	priority = 1000, -- needs to be loaded in first
@@ -21,6 +31,10 @@ completion["rachartier/tiny-inline-diagnostic.nvim"] = {
 					enabled = true,
 				},
 			},
+		})
+		vim.diagnostic.config({
+			underline = false,
+			virtual_text = false,
 		})
 	end,
 }
--- plugins/ui.lua
+++ plugins/ui.lua
@@ -1,3 +1,11 @@
 local ui = {}
 
+ui["Wansmer/symbol-usage.nvim"] = {
+	lazy = true,
+	event = "LspAttach",
+	config = function()
+		require("symbol-usage").setup()
+	end,
+}
+
 return ui
