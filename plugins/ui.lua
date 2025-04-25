5772b6edaac7b6ce9157e0432921ca80958d76ea
ayamir <lgt986452565@gmail.com>
2025-04-25 10:25:29 +0800

    update with colorful winsep.

--- plugins/ui.lua
+++ plugins/ui.lua
@@ -1,10 +1,26 @@
 local ui = {}
 
-ui["Wansmer/symbol-usage.nvim"] = {
+ui["nvim-zh/colorful-winsep.nvim"] = {
 	lazy = true,
-	event = "LspAttach",
+	event = "WinLeave",
 	config = function()
-		require("symbol-usage").setup()
+		require("colorful-winsep").setup({
+			hi = {
+				bg = "#2b3339",
+				fg = "#83c092",
+			},
+			no_exec_files = { "packer", "TelescopePrompt", "mason", "CompetiTest", "NvimTree" },
+			symbols = { "━", "┃", "┏", "┓", "┗", "┛" },
+			only_line_seq = true,
+			smooth = true,
+			exponential_smoothing = true,
+			anchor = {
+				left = { height = 1, x = -1, y = -1 },
+				right = { height = 1, x = -1, y = 0 },
+				up = { width = 0, x = -1, y = 0 },
+				bottom = { width = 0, x = 1, y = 0 },
+			},
+		})
 	end,
 }
 
