3e0d067f36c7b0b26444cfbfa114e08319f53f0c
ayamir <lgt986452565@gmail.com>
2025-04-28 13:00:39 +0800

    remove colorful winsep.

--- plugins/ui.lua
+++ plugins/ui.lua
@@ -1,27 +1,3 @@
 local ui = {}
 
-ui["nvim-zh/colorful-winsep.nvim"] = {
-	lazy = true,
-	event = "WinLeave",
-	config = function()
-		require("colorful-winsep").setup({
-			hi = {
-				bg = "#2b3339",
-				fg = "#83c092",
-			},
-			no_exec_files = { "packer", "TelescopePrompt", "mason", "CompetiTest", "NvimTree" },
-			symbols = { "━", "┃", "┏", "┓", "┗", "┛" },
-			only_line_seq = true,
-			smooth = true,
-			exponential_smoothing = true,
-			anchor = {
-				left = { height = 1, x = -1, y = -1 },
-				right = { height = 1, x = -1, y = 0 },
-				up = { width = 0, x = -1, y = 0 },
-				bottom = { width = 0, x = 1, y = 0 },
-			},
-		})
-	end,
-}
-
 return ui
