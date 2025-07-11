5c871b796969ab3ac462d0f5c97b750f0f15521d
ayamir <lgt986452565@gmail.com>
2025-07-11 14:44:51 +0800

    disable toggle term for nvchad.

--- configs/dropbar.lua
+++ configs/dropbar.lua
@@ -11,17 +11,5 @@ return {
 				return vim.fn.expand("%:p:h")
 			end,
 		},
-		terminal = {
-			name = function(buf)
-				local name = vim.api.nvim_buf_get_name(buf)
-				local term = select(2, require("toggleterm.terminal").identify(name))
-				-- Trying to "snag" a display name from toggleterm
-				if term then
-					return term.display_name or term.name
-				else
-					return name
-				end
-			end,
-		},
 	},
 }
