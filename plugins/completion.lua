b8cc51c782f4c5a22aceee040f04ed18046e0618
ayamir <lgt986452565@gmail.com>
2025-07-11 14:53:29 +0800

    remove garbage-day.

--- plugins/completion.lua
+++ plugins/completion.lua
@@ -1,19 +1,8 @@
 local completion = {}
-local global = require("core.global")
 
 completion["git@code.byted.org:chenjiaqi.cposture/codeverse.vim.git"] = {
 	config = function()
 		require("trae").setup()
-	end,
-}
-completion["ayamir/garbage-day.nvim"] = {
-	lazy = true,
-	event = "LspAttach",
-	config = function()
-		require("modules.utils").load_plugin("garbage-day", {
-			excluded_lsp_clients = { "null-ls" },
-			notifications = true,
-		})
 	end,
 }
 completion["Wansmer/symbol-usage.nvim"] = {
