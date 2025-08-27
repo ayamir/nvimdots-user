9cb92e894925bf3f56320fccf4bed07095dca75f
ayamir <lgt986452565@gmail.com>
2025-08-27 17:36:26 +0800

    remove normal-cmdline

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -20,29 +20,5 @@ editor["kylechui/nvim-surround"] = {
 		require("nvim-surround").setup()
 	end,
 }
-editor["jake-stewart/normal-cmdline.nvim"] = {
-	event = "CmdlineEnter",
-	config = function()
-		-- make the cmdline insert mode a beam
-		vim.opt.guicursor:append("ci:ver1,c:ver1")
-
-		local cmd = require("normal-cmdline")
-		cmd.setup({
-			-- key to hit within cmdline to enter normal mode:
-			key = "<esc>",
-			-- the cmdline text highlight when in normal mode:
-			hl = "Normal",
-			-- these mappings only apply to normal mode in cmdline:
-			mappings = {
-				["k"] = cmd.history.prev,
-				["j"] = cmd.history.next,
-				["<cr>"] = cmd.accept,
-				["<esc>"] = cmd.cancel,
-				["<c-c>"] = cmd.cancel,
-				[":"] = cmd.reset,
-			},
-		})
-	end,
-}
 
 return editor
