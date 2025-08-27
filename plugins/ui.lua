6f208353c471dfbdd17f27860e1a85079a4bd542
ayamir <lgt986452565@gmail.com>
2025-08-27 16:40:53 +0800

    use garbage-day to save lsp memory

--- plugins/ui.lua
+++ plugins/ui.lua
@@ -59,11 +59,7 @@ ui["folke/noice.nvim"] = {
 		},
 	},
 	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
 		"rcarriga/nvim-notify",
 	},
 }
