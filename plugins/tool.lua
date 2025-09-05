4fa3ddefe2c1fa98f34aab7b41d1603cc31ed228
ayamir <lgt986452565@gmail.com>
2025-09-05 11:28:40 +0800

    add normal cmdline

--- plugins/tool.lua
+++ plugins/tool.lua
@@ -76,5 +76,29 @@ tool["ayamir/bookmarks.nvim"] = {
 	},
 	opts = {},
 }
+tool["jake-stewart/normal-cmdline.nvim"] = {
+	event = "CmdlineEnter",
+	config = function()
+		-- make the cmdline insert mode a beam
+		vim.opt.guicursor:append("ci:ver1,c:ver1")
+
+		local cmd = require("normal-cmdline")
+		cmd.setup({
+			-- key to hit within cmdline to enter normal mode:
+			key = "<esc>",
+			-- the cmdline text highlight when in normal mode:
+			hl = "Normal",
+			-- these mappings only apply to normal mode in cmdline:
+			mappings = {
+				["k"] = cmd.history.prev,
+				["j"] = cmd.history.next,
+				["<cr>"] = cmd.accept,
+				["<esc>"] = cmd.cancel,
+				["<c-c>"] = cmd.cancel,
+				[":"] = cmd.reset,
+			},
+		})
+	end,
+}
 
 return tool
