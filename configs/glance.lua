d8288e9e4e6255a0baf27490766aa44bce7e4b95
ayamir <lgt986452565@gmail.com>
2025-09-11 11:59:56 +0800

    update user config

--- configs/glance.lua
+++
@@ -1,5 +0,0 @@
-return {
-	folds = {
-		folded = false,
-	},
-}
--- keymap/completion.lua
+++ keymap/completion.lua
@@ -1,9 +1,6 @@
 local mappings = {}
 local map_cr = require("keymap.bind").map_cr
 local map_callback = require("keymap.bind").map_callback
-local use_fzf = require("core.settings").search_backend == "fzf"
-local prompt_position = require("telescope.config").values.layout_config.horizontal.prompt_position
-local fzf_opts = { ["--layout"] = prompt_position == "top" and "reverse" or "default" }
 
 -- Place global keymaps here.
 mappings["plug_map"] = {}
@@ -19,32 +16,6 @@ mappings["lsp"] = function(buf)
 			:with_silent()
 			:with_buffer(buf)
 			:with_desc("lsp: Goto type definition"),
-		["n|gh"] = map_callback(function()
-				if use_fzf then
-					require("fzf-lua").lsp_finder({
-						fzf_opts = fzf_opts,
-					})
-				else
-					require("telescope.builtin").lsp_references()
-				end
-			end)
-			:with_noremap()
-			:with_nowait()
-			:with_silent()
-			:with_desc("lsp: show finder"),
-		["n|gm"] = map_callback(function()
-				if use_fzf then
-					require("fzf-lua").lsp_implementations({
-						fzf_opts = fzf_opts,
-					})
-				else
-					require("telescope.builtin").lsp_implementations()
-				end
-			end)
-			:with_noremap()
-			:with_nowait()
-			:with_silent()
-			:with_desc("lsp: show implementations"),
 
 		["n|gy"] = map_callback(function()
 				require("symbol-usage").refresh()
