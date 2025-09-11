940b5c9bc4de1b5a806bca7de94874b5ed021fc4
ayamir <lgt986452565@gmail.com>
2025-09-11 13:41:22 +0800

    support project level bookmarks

--- plugins/tool.lua
+++ plugins/tool.lua
@@ -1,5 +1,30 @@
 local tool = {}
 
+local find_or_create_project_bookmark_group = function()
+	local project_root = require("project").get_project_root()
+	if not project_root then
+		return
+	end
+
+	local project_name = string.gsub(project_root, "^" .. os.getenv("HOME") .. "/", "")
+	local Service = require("bookmarks.domain.service")
+	local Repo = require("bookmarks.domain.repo")
+	local bookmark_list = nil
+
+	for _, bl in ipairs(Repo.find_lists()) do
+		if bl.name == project_name then
+			bookmark_list = bl
+			break
+		end
+	end
+
+	if not bookmark_list then
+		bookmark_list = Service.create_list(project_name)
+	end
+	Service.set_active_list(bookmark_list.id)
+	require("bookmarks.sign").safe_refresh_signs()
+end
+
 tool["fei6409/log-highlight.nvim"] = {
 	lazy = true,
 	event = "VeryLazy",
@@ -74,7 +99,15 @@ tool["ayamir/bookmarks.nvim"] = {
 		{ "nvim-telescope/telescope.nvim" }, -- currently has only telescopes supported, but PRs for other pickers are welcome
 		{ "stevearc/dressing.nvim" }, -- optional: better UI
 	},
-	opts = {},
+	config = function()
+		require("bookmarks").setup({})
+
+		vim.api.nvim_create_autocmd({ "VimEnter", "BufEnter" }, {
+			group = vim.api.nvim_create_augroup("BookmarksGroup", {}),
+			pattern = { "*" },
+			callback = find_or_create_project_bookmark_group,
+		})
+	end,
 }
 tool["jake-stewart/normal-cmdline.nvim"] = {
 	event = "CmdlineEnter",
