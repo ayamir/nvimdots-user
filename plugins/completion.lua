7256e708cc8764a89bd3099060a220a3dcde4ac2
ayamir <lgt986452565@gmail.com>
2025-09-24 14:02:06 +0800

    update config

--- plugins/completion.lua
+++ plugins/completion.lua
@@ -5,85 +5,21 @@ completion["git@code.byted.org:chenjiaqi.cposture/codeverse.vim.git"] = {
 		require("trae").setup()
 	end,
 }
-completion["ayamir/garbage-day.nvim"] = {
-	lazy = true,
-	event = "LspAttach",
-	config = function()
-		require("garbage-day").setup({
-			excluded_lsp_clients = { "null-ls" },
-			notifications = true,
-		})
-	end,
-}
+-- completion["ayamir/garbage-day.nvim"] = {
+-- 	lazy = true,
+-- 	event = "LspAttach",
+-- 	config = function()
+-- 		require("garbage-day").setup({
+-- 			excluded_lsp_clients = { "null-ls" },
+-- 			notifications = true,
+-- 		})
+-- 	end,
+-- }
 completion["Wansmer/symbol-usage.nvim"] = {
 	lazy = true,
 	event = "LspAttach",
 	config = function()
-		local function h(name)
-			return vim.api.nvim_get_hl(0, { name = name })
-		end
-
-		-- hl-groups can have any name
-		vim.api.nvim_set_hl(0, "SymbolUsageRounding", { fg = h("CursorLine").bg, italic = true })
-		vim.api.nvim_set_hl(0, "SymbolUsageContent", { bg = h("CursorLine").bg, fg = h("Comment").fg, italic = true })
-		vim.api.nvim_set_hl(0, "SymbolUsageRef", { fg = h("Function").fg, bg = h("CursorLine").bg, italic = true })
-		vim.api.nvim_set_hl(0, "SymbolUsageDef", { fg = h("Type").fg, bg = h("CursorLine").bg, italic = true })
-		vim.api.nvim_set_hl(0, "SymbolUsageImpl", { fg = h("@keyword").fg, bg = h("CursorLine").bg, italic = true })
-
-		local function text_format(symbol)
-			local res = {}
-
-			local round_start = { "", "SymbolUsageRounding" }
-			local round_end = { "", "SymbolUsageRounding" }
-
-			-- Indicator that shows if there are any other symbols in the same line
-			local stacked_functions_content = symbol.stacked_count > 0 and ("+%s"):format(symbol.stacked_count) or ""
-
-			if symbol.references then
-				local usage = symbol.references <= 1 and "usage" or "usages"
-				local num = symbol.references == 0 and "no" or symbol.references
-				table.insert(res, round_start)
-				table.insert(res, { "󰌹 ", "SymbolUsageRef" })
-				table.insert(res, { ("%s %s"):format(num, usage), "SymbolUsageContent" })
-				table.insert(res, round_end)
-			end
-
-			if symbol.definition then
-				if #res > 0 then
-					table.insert(res, { " ", "NonText" })
-				end
-				table.insert(res, round_start)
-				table.insert(res, { "󰳽 ", "SymbolUsageDef" })
-				table.insert(res, { symbol.definition .. " defs", "SymbolUsageContent" })
-				table.insert(res, round_end)
-			end
-
-			if symbol.implementation then
-				if #res > 0 then
-					table.insert(res, { " ", "NonText" })
-				end
-				table.insert(res, round_start)
-				table.insert(res, { "󰡱 ", "SymbolUsageImpl" })
-				table.insert(res, { symbol.implementation .. " impls", "SymbolUsageContent" })
-				table.insert(res, round_end)
-			end
-
-			if stacked_functions_content ~= "" then
-				if #res > 0 then
-					table.insert(res, { " ", "NonText" })
-				end
-				table.insert(res, round_start)
-				table.insert(res, { " ", "SymbolUsageImpl" })
-				table.insert(res, { stacked_functions_content, "SymbolUsageContent" })
-				table.insert(res, round_end)
-			end
-
-			return res
-		end
-
-		require("symbol-usage").setup({
-			text_format = text_format,
-		})
+		require("symbol-usage").setup({})
 
 		vim.api.nvim_create_user_command("E", function()
 			require("symbol-usage").refresh()
