8f8b70e53b0b6b76d577bfaa26d5dd2030d6b8bb
ayamir <lgt986452565@gmail.com>
2025-10-22 14:57:11 +0800

    add fold plugin

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -20,5 +20,17 @@ editor["kylechui/nvim-surround"] = {
 		require("nvim-surround").setup()
 	end,
 }
+editor["chrisgrieser/nvim-origami"] = {
+	event = "VeryLazy",
+	opts = {
+		autoFold = { enabled = false },
+	}, -- needed even when using default config
+
+	-- recommended: disable vim's auto-folding
+	init = function()
+		vim.opt.foldlevel = 99
+		vim.opt.foldlevelstart = 99
+	end,
+}
 
 return editor
