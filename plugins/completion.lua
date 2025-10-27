e191167a244921a03969fa2d7d013ec2983b0e0d
ayamir <lgt986452565@gmail.com>
2025-10-27 19:23:35 +0800

    enable garbage-day

--- plugins/completion.lua
+++ plugins/completion.lua
@@ -5,16 +5,16 @@ completion["git@code.byted.org:chenjiaqi.cposture/codeverse.vim.git"] = {
 		require("trae").setup()
 	end,
 }
--- completion["ayamir/garbage-day.nvim"] = {
--- 	lazy = true,
--- 	event = "LspAttach",
--- 	config = function()
--- 		require("garbage-day").setup({
--- 			excluded_lsp_clients = { "null-ls" },
--- 			notifications = true,
--- 		})
--- 	end,
--- }
+completion["ayamir/garbage-day.nvim"] = {
+	lazy = true,
+	event = "LspAttach",
+	config = function()
+		require("garbage-day").setup({
+			excluded_lsp_clients = { "null-ls" },
+			notifications = true,
+		})
+	end,
+}
 completion["Wansmer/symbol-usage.nvim"] = {
 	lazy = true,
 	event = "LspAttach",
