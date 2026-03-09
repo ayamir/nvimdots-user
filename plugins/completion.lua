7bfffef0301a372e607c31b484dfb5114732ceed
ayamir <lgt986452565@gmail.com>
2026-03-09 22:21:55 +0800

    remove codeverse

--- plugins/completion.lua
+++ plugins/completion.lua
@@ -1,10 +1,5 @@
 local completion = {}
 
-completion["git@code.byted.org:chenjiaqi.cposture/codeverse.vim.git"] = {
-	config = function()
-		require("trae").setup()
-	end,
-}
 completion["ayamir/garbage-day.nvim"] = {
 	lazy = true,
 	event = "LspAttach",
