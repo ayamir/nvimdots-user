53a17afeb12ffc926a7c78e2bf04b8c01b41039b
ayamir <lgt986452565@gmail.com>
2026-04-06 22:41:01 +0800

    update

--- keymap/core.lua
+++ keymap/core.lua
@@ -2,6 +2,6 @@ local bind = require("keymap.bind")
 local map_cmd = bind.map_cmd
 
 return {
-	["nv|j"] = map_cmd("gj"),
-	["nv|k"] = map_cmd("gk"),
+	["nv|j"] = map_cmd("gj"):with_noremap(),
+	["nv|k"] = map_cmd("gk"):with_noremap(),
 }
