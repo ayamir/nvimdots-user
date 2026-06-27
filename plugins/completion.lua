5b2248e0e5ef3e6a7ecffbb72429e10aec2a06b4
ayamir <lgt986452565@gmail.com>
2026-06-27 22:43:36 +0800

    remove unused plugins

--- plugins/completion.lua
+++ plugins/completion.lua
@@ -1,20 +1,5 @@
 local completion = {}
 
-completion["git@code.byted.org:chenjiaqi.cposture/codeverse.vim.git"] = {
-	config = function()
-		require("trae").setup()
-	end,
-}
-completion["ayamir/garbage-day.nvim"] = {
-	lazy = true,
-	event = "LspAttach",
-	config = function()
-		require("garbage-day").setup({
-			excluded_lsp_clients = { "null-ls" },
-			notifications = true,
-		})
-	end,
-}
 completion["Wansmer/symbol-usage.nvim"] = {
 	lazy = true,
 	event = "LspAttach",
